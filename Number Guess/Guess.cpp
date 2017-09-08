cb9985fa34c202e2536532f15f58583d4560d93f
Richard Leinfellner <richard@leinfellner.com>
2017-09-08 14:02:26 +0100

    Add project files.

---
+++ Number Guess/Guess.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+
+int main(int argc, char ** argv) {
+
+	//DToDo Loop while we have not guessed number
+		//ToDo Make up current guess, half way between low and high
+		//ToDo Show current guess
+		//ToDo Get User Input (correct, high, low)
+		//ToDo If correct Break loop
+		//ToDo If too low set new High limit
+		//ToDo If too high set new Low limit
+
+	return	0;
+}
